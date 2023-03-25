d03a7931475df217f1a8452a5a1edb77702333d4
Anastasia Horbenko <anastasiakopylova00@gmail.com>
2023-03-25 11:05:10 +0200

    Do task promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const formElement = document.querySelector('.form');
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  const { delay, step, amount } = getFormValues(formElement);
+  handlePromises(delay, step, amount);
+});
+function getFormValues(form) {
+  const delayInput = form.querySelector('input[name="delay"]');
+  const stepInput = form.querySelector('input[name="step"]');
+  const amountInput = form.querySelector('input[name="amount"]');
+  return {
+    delay: parseInt(delayInput.value),
+    step: parseInt(stepInput.value),
+    amount: parseInt(amountInput.value),
+  };
+}
+async function handlePromises(delay, step, amount) {
+  for (let i = 1; i <= amount; i += 1) {
+    try {
+      const result = await createPromise(i, delay + (i - 1) * step);
+      handlePromiseSuccess(result);
+    } catch (error) {
+      handlePromiseError(error);
+    }
   }
+}
+function handlePromiseSuccess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+}
+function handlePromiseError({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
