d4a8e697e99dddbb2177303cede105a11cf8cfc1
Anastasia Horbenko <anastasiakopylova00@gmail.com>
2023-03-25 09:59:34 +0200

    Update color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,39 +1,43 @@
-import { refs } from './01-color-switcher';
-export function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const controls = { 
+  start: document.querySelector('[data-start]'), 
+  stop: document.querySelector('[data-stop]'), 
+}; 
+ 
+let colorSwitchIntervalId = null; 
+let currentColor = null; 
+ 
+setControlsState(false); 
+ 
+controls.start.addEventListener('click', startColorSwitchInterval); 
+controls.stop.addEventListener('click', stopColorSwitchInterval); 
+ 
+function startColorSwitchInterval() { 
+  setControlsState(true); 
+ 
+  colorSwitchIntervalId = setInterval(() => { 
+    currentColor = getRandomHexColor(); 
+    setBodyBackgroundColor(currentColor); 
+  }, 1000); 
+} 
+ 
+function stopColorSwitchInterval() { 
+  setControlsState(false); 
+ 
+  clearInterval(colorSwitchIntervalId); 
+  if (currentColor) setBodyBackgroundColor(currentColor); 
+} 
+ 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-export function setBodyBgColor(color) {
-  document.body.style.backgroundColor = color;
-}
-export function setColorSwitchState(state) {
-  refs.startBtn.disabled = state;
-  refs.stopBtn.disabled = !state;
-}
-export const refs = {
-  startBtn: document.querySelector('[data-start]'),
-  stopBtn: document.querySelector('[data-stop]'),
-};
-let colorSwitchIntervalId = null;
-let currentColor = null;
-setColorSwitchState(false);
-
-refs.startBtn.addEventListener('click', startColorSwitch);
-refs.stopBtn.addEventListener('click', stopColorSwitch);
-
-function startColorSwitch() {
-  setColorSwitchState(true);
-
-  colorSwitchIntervalId = setInterval(() => {
-    currentColor = getRandomHexColor();
-    setBodyBgColor(currentColor);
-  }, 1000);
-}
-
-function stopColorSwitch() {
-  setColorSwitchState(false);
-
-  clearInterval(colorSwitchIntervalId);
-  if (currentColor) setBodyBgColor(currentColor);
-}
+ 
+function setBodyBackgroundColor(color) { 
+  document.body.style.backgroundColor = color; 
+} 
+ 
+function setControlsState(state) { 
+  controls.start.disabled = state; 
+  controls.stop.disabled = !state; 
+} 
+ 
+export { controls, startColorSwitchInterval, stopColorSwitchInterval };
